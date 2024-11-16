6174545b40d572ec8a02004abfac62b1efcf05e3
Ohith <ohithprasanna1532003@gmail.com>
2024-11-16 20:19:01 +0530

    Day 1:Objects & Constructor function

--- Obects & Constructor Functions.js
+++ Obects & Constructor Functions.js
@@ -10,6 +10,7 @@ console.log(employee.name);
    --If object key has sapce and enclosed within double quotes,it requires the key to be mentioned within double quotes enclosed by []
    --deleting an entity=Delete.entity name 
    --After declaring object adding entity requires[] and single quotes is mandiatory
+   --In js For objects it doest need a class
 */
 
 /*
@@ -42,4 +43,32 @@ function a(b,c,d,f,g){
 
 }
 let o=new a('Ohith',21,5000000,"Tester","Good")
+let j=new a("kevin",35,60000,"developer","bad")
+
+console.log(j.description());
 console.log(o.description());
+
+//To print every single values in an Object (in) can be used 
+for(key in o){
+    console.log(`${key}-${o[key]}`)
+}
+
+//console.log(j.description('good')); //If this is not given in the return statement and function is passed with parameter it takes 
+//argument inside the function
+/*
+function a(b,c,d,f,g){
+    this.name=b,
+    this.age=c,
+    this.salary=d,
+    this.designation=f,
+    this.behavior=g,
+    this.description=(behavior)=>{
+        return `${this.name} is a ${behavior} being`;
+    }
+
+}
+let j1=new a("kevin",35,60000,"developer","bad")
+console.log(j1.description('good'));
+*/
+
+//To print every single values in an Object in can be used 

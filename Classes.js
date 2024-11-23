6c8012a876cf73e37ec41b4e5a2f9721162da434
Ohith <ohithprasanna1532003@gmail.com>
2024-11-23 19:59:56 +0530

    Day 3: Classes and static variables & methods

--- Classes.js
+++ Classes.js
@@ -29,7 +29,10 @@ console.log(o.model)
 /*Static variable or method belongs to the class rather than instance of class(object)
 if a variable or method is static then class can be the only entity to access it  */
 
+/*
 class car{
+
+    static price=20000 //belongs to class car
     constructor(a,b,speed){
         this.model=a,
         this.price=b,
@@ -41,12 +44,41 @@ class car{
     }
     //static model='kia'
 
-     looks(){
-        return `${this.model} is great looking  With ${this.c} top Speed and its great`
+    static looks(model,c){
+        return `${model} is great looking  With ${c} top Speed and its great with price ${this.price}`
+        //this.price refers to car.price as its static variable
+
     }
 
 }
 
 let o= new car("Nissan",1000000,"20000cc");
 console.log(o.performance("badest"))
-console.log(car.looks())
+console.log(car.looks('Kia','250'))//if the method is Static so we use class name
+
+*/
+
+class car{
+
+    static price=20000 //belongs to class car
+    constructor(a,b,speed){
+        this.model=a,
+        this.price=b,
+        this.c=speed,
+       this.performance=function(c){
+            return `the ${this.model}'s tops speed is ${this.c} so its a ${c} car `
+
+        }
+    }
+
+    static looks(model,c){
+        return `${model} is great looking  With ${c} top Speed and its great with price ${this.price}`
+        
+
+    }
+
+}
+
+let o= new car("Nissan",1000000,"20000cc");
+console.log(o.performance("badest"))
+console.log(car.looks(o.model,o.c))//if u like to use the object parameter in static method we can use class.method(object.parameter)
